7203b6292e405def084adf3e5e1b242438b97a36
Catalina Cavallo <catalinacavallo10@gmail.com>
2025-05-07 07:43:50 -0300

    Merge branch 'master' of https://github.com/Serena-code/appcompra into VistaArtista

--- src/pages/DetalleAlbum.jsx
+++ src/pages/DetalleAlbum.jsx
@@ -1,34 +1,37 @@
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-function DetalleAlbum (){
-  const [detalle, setDetalle] = useState([])
-  function buscarDetalle(detalle){
-    axios.get(`https://api.spotify.com/v1/albums`,
-    {headers: {
-      Authorization: `Bearer ${token}`,
-    },params: {
-      q: detalle,
-      type: "albums",
-    },
-  })
+function Album({ idArtista, token }) {
+  const [album, setAlbum] = useState([])
+
+  function buscarAlbum(id) {
+    axios.get(`https://api.spotify.com/v1/artists/${id}/albums`, {
+      headers: { Authorization: `Bearer ${token}` }
+    })
     .then((data) => {
-      console.log(data.data.detalle.items)
-      setDatalle(data.data.detalle.items)
-    }).catch((error) => {
+      setAlbum(data.data.items)
+    })
+    .catch((error) => {
       console.log(error)
     })
   }
-        
+
+  useEffect(() => {
+    if (idArtista && token) {
+      buscarAlbum(idArtista)
+    }
+  }, [idArtista, token])
+
   return (
-    <div className="DetalleAlbum">
-    <h1>Detalle Album</h1>
-    <ul>
-      {detalle.map((detalle, index) => {
-        return <li key={index}>{detalle.name}</li>
-      })}
-    </ul>
+    <div className="Album">
+      <h1>Albumes</h1>
+      <ul>
+        {album.map((album, index) => (
+          <li key={index}>{album.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
 
-export default DetalleAlbum
+export default Album
