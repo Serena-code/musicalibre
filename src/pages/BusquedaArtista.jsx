b2c3570ff52cbfd9f697009d033a076939195270
Catalina Cavallo <catalinacavallo10@gmail.com>
2025-05-11 10:49:50 -0300

    solucion-token

--- src/pages/BusquedaArtista.jsx
+++ src/pages/BusquedaArtista.jsx
@@ -1,90 +1,89 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import {DetalleArtista} from '../componentes/DetalleArtista'
-import { useParams,Link } from 'react-router-dom'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { DetalleArtista } from '../componentes/DetalleArtista';
+import { useParams, Link } from 'react-router-dom';
+
+// Variable global para el token (persiste al navegar)
+let spotifyToken = null;
 
 export function BusquedaArtista() {
-  const CLIENT_ID = "6b78425f682040b4a31811c058a3e765"
-  const CLIENT_SECRET = "1da5735ec4c94ea290ee06994f65ad09"
+  const CLIENT_ID = "6b78425f682040b4a31811c058a3e765";
+  const CLIENT_SECRET = "1da5735ec4c94ea290ee06994f65ad09";
 
-  const[artistaSeleccionado,setArtistaSeleccionado] = useState("")
-  const{nombreArtista} = useParams()
-  const [artista, setArtista] = useState([])
-  
-  
+  const [artistaSeleccionado, setArtistaSeleccionado] = useState(null);
+  const { nombreArtista } = useParams();
+  const [artistas, setArtistas] = useState([]);
+
   function pedirToken() {
+    if (spotifyToken) {
+      axios.defaults.headers.common['Authorization'] = `Bearer ${spotifyToken}`;
+      buscarArtista();
+      return;
+    }
 
-    axios.post("https://accounts.spotify.com/api/token",
-      {
-        grant_type:"client_credentials",
-        client_id: CLIENT_ID,
-        client_secret: CLIENT_SECRET
-      },
+    axios.post(
+      "https://accounts.spotify.com/api/token",
+      `grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`,
       {
         headers: {
-         "Content-Type": "application/x-www-form-urlencoded"
-        },
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
       }
-      
     )
-    .then((data) => {
-      console.log(`Token recibido: ${data.data.access_token}`)
-      axios.defaults.headers.common['Authorization'] = "Bearer " + data.data.access_token;
-      buscarArtista()
-      
+    .then(({ data }) => {
+      spotifyToken = data.access_token;
+      console.log(`Token recibido: ${spotifyToken}`);
+      axios.defaults.headers.common['Authorization'] = `Bearer ${spotifyToken}`;
+      buscarArtista();
     })
     .catch((error) => {
-      console.log(`Error: ${error}`)
-    })
-    
+      console.error("Error al obtener token:", error);
+      spotifyToken = null; 
+    });
   }
 
   function buscarArtista() {
-    
-    axios.get(`https://api.spotify.com/v1/search?q=${nombreArtista}&type=artist`, {
-    })
-    .then((data) => {
-      console.log(data.data.artists.items)
-      setArtista(data.data.artists.items)
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-    console.log(artista)
+    axios.get(`https://api.spotify.com/v1/search?q=${nombreArtista}&type=artist`)
+      .then(({ data }) => {
+        setArtistas(data.artists.items);
+      })
+      .catch((error) => {
+        console.error("Error al buscar artistas:", error);
+        if (error.response?.status === 401) {
+          spotifyToken = null; 
+        }
+      });
   }
 
   useEffect(() => {
-    pedirToken()
-
-  }, [])
-
- 
+    if (nombreArtista) {
+      pedirToken();
+    }
+  }, [nombreArtista]);
 
   return (
     <div className="BusquedaArtista">
-      {!artistaSeleccionado && (
+      {!artistaSeleccionado ? (
         <>
           <h1>Artistas</h1>
-          <Link to={"/"} className='btn'>
+          <Link to="/" className="btn">
             Volver
           </Link>
           <ul>
-            {artista.map((artista, index) => (
-            <li key={index} onClick={() => setArtistaSeleccionado(artista)}>
-              <h1>{artista.name}</h1>
-              <img src={artista.images?.[0].url} alt={artista.name} />
-            </li>
+            {artistas.map((artista, index) => (
+              <li key={index} onClick={() => setArtistaSeleccionado(artista)}>
+                <h1>{artista.name}</h1>
+                <img
+                  src={artista.images?.[0]?.url || "https://picsum.photos/150"}
+                  alt={artista.name}
+                />
+              </li>
             ))}
           </ul>
         </>
+      ) : (
+        <DetalleArtista artista={artistaSeleccionado} />
       )}
-  
-      {artistaSeleccionado && (
-        <div>
-          <DetalleArtista artista={artistaSeleccionado} />              
-        </div>
-      )}
-  
     </div>
   );
 }
